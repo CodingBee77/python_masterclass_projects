09dcb40fb4d7bd220c568c484588c824d9336c9a
martyna <codingbee77@tlen.pl>
2025-03-10 12:09:37 +0100

    Regular expression workbook - caret metacharacter

---
+++ caret_metacharacter.py
@@ -0,0 +1,10 @@
+import re
+
+#Caret metacharacter means that stirng needs to start with a certain string
+string="9123456765342" # is a match
+string_2 = "92834975892474932" # not a match
+pattern = r"^91"
+if re.match(pattern,string):
+    print("Match found")
+else:
+    print("No match found")
