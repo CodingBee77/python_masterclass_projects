6398d6522a8b7486d2846c09de69fbd26b2a4b40
martyna <codingbee77@tlen.pl>
2025-03-26 09:46:54 +0100

    Regular expression exercises - Add comments

--- regular_expressions/shorthand_for_numeric_char.py
+++ regular_expressions/shorthand_for_numeric_char.py
@@ -3,10 +3,10 @@ import re
 sentence = "The meeting is scheduled at 9 AM."
 # Shorthand for finding numbers, equivalent to [0-9]
 pattern = r"\d"
-numeric_characters = re.findall(pattern, sentence)
-print(numeric_characters)
+digit_characters = re.findall(pattern, sentence)
+print(f"Digit characters using \d : {digit_characters}.")
 
 # Shorthand for finding non digit characters from 0-9
 pattern_non_digit = r"\D"
 non_digit_characters = re.findall(pattern_non_digit, sentence)
-print(non_digit_characters)
+print(f"Nondigit characters using \d : {non_digit_characters}.")
